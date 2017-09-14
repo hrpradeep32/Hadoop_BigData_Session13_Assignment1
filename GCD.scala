142ccd50a1ff4f9dd4b47e4d3e8de7e8ae8bad96
hrpradeep32 <hrpradeep32@gmail.com>
2017-09-14 23:14:05 +0530

    Add files via upload

---
+++ GCD.scala
@@ -0,0 +1,29 @@
+
+
+package Acad
+
+
+
+object GCD 
+{
+  
+  
+
+	def gcd(a:Int,b:Int):Int={
+    
+
+		if(b==0) a else gcd(b, a%b)
+  
+}
+  
+  
+
+def main(args:Array[String])
+	{
+    
+		println(gcd(12,20))
+  
+	}
+  
+
+}
